872dc2fe168f0db17006ae24ea6e6f3e1885dac8
MRicsi1211 <miklosrichard2002@gmail.com>
2023-09-14 12:57:02 +0200

    BasicInfo kész

---
+++ BasicInfo.java
@@ -0,0 +1,19 @@
+public class BasicInfo {
+    public static void main(String[] args) {
+
+
+        String name = "Miklós Richárd";
+        int age = 21;
+        double heightInMeters = 1.88;
+        boolean isMarried = false;
+
+
+        System.out.println("Név: " + name);
+        System.out.println("Kor: " + age);
+        System.out.println("Magasság(méterbe): " + heightInMeters);
+        System.out.println("Házas: " + isMarried);
+
+
+
+    }
+}
