046f62aa9db0ab35ee6958a30e838bbc1510d3b0
Miklós Richárd Gábor <115459581+szt2mikric@users.noreply.github.com>
2023-09-22 09:10:31 +0200

    Update DrawTriangle.java

--- DrawTriangle.java
+++ DrawTriangle.java
@@ -21,7 +21,7 @@ public class DrawTriangle {
             for (int j = 1; j <= i; j++) {
                 System.out.print("*");
             }
-            System.out.println(); // Move to the next line after each row
+            System.out.println(); 
         }
     }
 }
