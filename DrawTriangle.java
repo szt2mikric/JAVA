774d60a66cd6b8f6e2e6f2ee44c4836a8d976d4d
Miklós Richárd Gábor <115459581+szt2mikric@users.noreply.github.com>
2023-09-22 09:09:16 +0200

    DrawTriangle

---
+++ DrawTriangle.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class DrawTriangle {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Please enter the triangle height: ");
+        int height = scanner.nextInt();
+        
+        if (height <= 0) {
+            System.out.println("Triangle height must be a positive integer.");
+        } else {
+            drawTriangle(height);
+        }
+        
+        scanner.close();
+    }
+    
+    public static void drawTriangle(int height) {
+        for (int i = 1; i <= height; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println(); // Move to the next line after each row
+        }
+    }
+}
